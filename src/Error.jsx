60518ff8735c0f7ff377c4736b0c0c45144d47eb
Mudassir Shahid <mudassirshahid84@gmail.com>
2022-10-21 16:21:42 +0500

    second commit

--- src/Error.jsx
+++ src/Error.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Error = () => {
     return (
       <>
-        <h1 className='mt-5' >Error</h1>
+        <h1 className='mt-5' >Error Page</h1>
       </>
     )
   }
